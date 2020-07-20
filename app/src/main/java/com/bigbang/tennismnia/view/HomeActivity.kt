cdd600359cf34e9f4ea791516a35304039a59cc3
DEV-707 <DEV-707@tennismania.chal>
2020-07-20 12:02:58 -0400

    Addition of lifecycle extensions

--- app/src/main/java/com/bigbang/tennismnia/view/HomeActivity.kt
+++ app/src/main/java/com/bigbang/tennismnia/view/HomeActivity.kt
@@ -2,11 +2,26 @@ package com.bigbang.tennismnia.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.bigbang.tennismnia.R
+import com.bigbang.tennismnia.viewmodel.TennisViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
+
+
+    private lateinit var tennisViewModel: TennisViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tennisViewModel = ViewModelProvider
+
+        start_game_button.setOnClickListener {
+
+
+        }
     }
 }
