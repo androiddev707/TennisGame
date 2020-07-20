6f0910d503a54e060fb57570ece4bed3b34ec6fd
DEV-707 <DEV-707@tennismania.chal>
2020-07-20 12:40:04 -0400

    Removed action bar and added fragment transaction

--- app/src/main/java/com/bigbang/tennismnia/view/HomeActivity.kt
+++ app/src/main/java/com/bigbang/tennismnia/view/HomeActivity.kt
@@ -1,12 +1,8 @@
 package com.bigbang.tennismnia.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.appcompat.app.AppCompatActivity
 import com.bigbang.tennismnia.R
 import com.bigbang.tennismnia.viewmodel.TennisViewModel
 import com.bigbang.tennismnia.viewmodel.TennisViewModelFactory
@@ -14,7 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
 
-
+    private val gameFragment: GameFragment by lazy { GameFragment() }
     private val tennisViewModel: TennisViewModel by viewModels(factoryProducer = { TennisViewModelFactory })
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +19,19 @@ class HomeActivity : AppCompatActivity() {
 
         start_game_button.setOnClickListener {
             tennisViewModel.startGame()
+            openGameFragment()
         }
+    }
+
+    private fun openGameFragment() {
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right,
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+            )
+            .replace(R.id.game_frame, gameFragment)
+            .commit()
     }
 }
