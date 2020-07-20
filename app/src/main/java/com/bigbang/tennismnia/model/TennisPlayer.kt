bfb89aa7110a15f9678a57c7c2454ec838bcecb2
DEV-707 <DEV-707@tennismania.chal>
2020-07-20 13:22:22 -0400

    Implemented Game Logic + game timer

--- app/src/main/java/com/bigbang/tennismnia/model/TennisPlayer.kt
+++ app/src/main/java/com/bigbang/tennismnia/model/TennisPlayer.kt
@@ -1,3 +1,13 @@
 package com.bigbang.tennismnia.model
 
-data class TennisPlayer(var playerName: String = "", var gamePoints: Int = 0)
+data class TennisPlayer(var playerName: String = "", var gamePoints: Int = 0) {
+    fun getScore(): String {
+        return when (gamePoints) {
+            0 -> "LOVE"
+            1 -> "15"
+            2 -> "30"
+            3 -> "40"
+            else -> "GAME"
+        }
+    }
+}
--- app/src/main/java/com/bigbang/tennismnia/view/GameFragment.kt
+++ app/src/main/java/com/bigbang/tennismnia/view/GameFragment.kt
@@ -1,6 +1,8 @@
 package com.bigbang.tennismnia.view
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +12,11 @@ import androidx.lifecycle.Observer
 import com.bigbang.tennismnia.R
 import com.bigbang.tennismnia.viewmodel.TennisViewModel
 import com.bigbang.tennismnia.viewmodel.TennisViewModelFactory
+import kotlinx.android.synthetic.main.game_fragment_layout.*
 
 class GameFragment : Fragment() {
 
+    private var gameTime = 0
     private val tennisViewModel: TennisViewModel by viewModels(factoryProducer = { TennisViewModelFactory })
 
     override fun onCreateView(
@@ -24,10 +28,41 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tennisViewModel.getGameData().observe(viewLifecycleOwner, Observer {
-            it.let {
-
+        val handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                updateTimer()
+                handler.postDelayed(this, 1000)
             }
+        }, 0)
+
+        tennisViewModel.getGameData().observe(viewLifecycleOwner, Observer { tennisGame ->
+            lead_player_text.text = tennisGame.getGameStatus()
+            player_one_score_textview.text = tennisGame.playerOne.getScore()
+            player_two_score_textview.text = tennisGame.playerTwo.getScore()
+
+            player_one_button.setOnClickListener { view ->
+                tennisGame.playerOne.gamePoints++
+                lead_player_text.text = tennisGame.getGameStatus()
+                player_one_score_textview.text = tennisGame.playerOne.getScore()
+            }
+
+            player_two_button.setOnClickListener { view ->
+                tennisGame.playerTwo.gamePoints++
+                lead_player_text.text = tennisGame.getGameStatus()
+                player_two_score_textview.text = tennisGame.playerTwo.getScore()
+            }
+
+
         })
     }
+
+    private fun updateTimer() {
+        gameTime++
+        val seconds = gameTime % 60
+        val minutes = (gameTime /60) % 60
+        game_timer_textview.text = "$minutes:$seconds"
+    }
+
+
 }
